c75d78606b1889ddcacb5ad0da395e0a877db4d4
Ali <90877977+AlhakeemALI@users.noreply.github.com>
2023-03-03 05:19:21 -0700

    Handling Comment Form Submission

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 
 from django.shortcuts import render,get_object_or_404
 from .models import Post
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView
 from . forms import CommentForm
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 
@@ -25,16 +28,40 @@ class AllPostView(ListView):
        ordering = ["-date"]
        context_object_name = "all_posts"
 
-class PostDetailView(DetailView):
-    template_name =  "blog/post-ditail.html"
-    model = Post
-    context_object_name = "singel_post"
+class PostDetailView(View):
+      # template_name =  "blog/post-ditail.html"
+      # model = Post
+      # context_object_name = "singel_post"
 
-    def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        context["tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+      def get(self,request,slug):
+          post= Post.objects.get(slug=slug)
+          context = {
+               "post": post,
+               "tags": post.tags.all(),
+               "comment_form": CommentForm()
+                
+
+          }
+          return render(request, "blog/post-ditail.html", context)
+         
+      def post(self,request,slug):
+         comment_form = CommentForm(request.POST)
+         post= Post.objects.get(slug=slug)
+         if comment_form.is_valid():
+             comment = comment_form.save(commit=False) 
+             comment.post = post
+             comment.save()
+             return HttpResponseRedirect(reverse("post-detail", args=[slug]))
+         post= Post.objects.get(slug=slug)
+         context = {
+               "post": post,
+               "tags": post.tags.all(),
+               "comment_form": CommentForm()
+         }
+         return render(request, "blog/post-ditail.html", context)
+         
+
+
 
 
     
